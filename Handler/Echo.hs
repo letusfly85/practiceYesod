e273491f3052bb8eb7e4a02a34ac00bc72e18427
shunsuke wada <letusfly95@gmail.com>
2014-07-22 02:21:03 +0900

    add-handler sample

---
+++ Handler/Echo.hs
@@ -0,0 +1,7 @@
+module Handler.Echo where
+
+import Import
+
+getEchoR :: Text -> Handler Html
+--getEchoR theText = defaultLayout [whamlet|<h1>#{theText}|]
+getEchoR theText = defaultLayout $(widgetFile "echo")
